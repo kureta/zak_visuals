e3c0a1f2fd17d832b35b1f0d2bc36e98152a8cc3
kureta <skureta@gmail.com>
2019-04-24 03:01:16 +0300

    I have no idea

--- berlin/model/autoencoder.py
+++ berlin/model/autoencoder.py
@@ -1,7 +1,60 @@
-import torch.nn as nn
+from functools import partial
 
-from berlin.model.decoder import Decoder
-from berlin.model.encoder import Encoder
+import torch.nn as nn
+from torch.nn import functional as F
+
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3,
+                               out_channels=64,
+                               kernel_size=3,
+                               padding=1)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=3,
+                                     stride=2,
+                                     padding=1)
+        self.conv2 = nn.Conv2d(in_channels=64,
+                               out_channels=128,
+                               kernel_size=3,
+                               padding=1)
+
+        self.relu = nn.LeakyReLU()
+        self.tanh = nn.Tanh()
+
+    def forward(self, image, audio):
+        z = self.conv1(image)
+        z = self.relu(z)
+        z = self.maxpool1(z)
+        z = self.conv2(z)
+        z = self.tanh(z)
+
+        return z
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=128,
+                               out_channels=64,
+                               kernel_size=3,
+                               padding=1)
+        self.conv2 = nn.Conv2d(in_channels=64,
+                               out_channels=3,
+                               kernel_size=3,
+                               padding=1)
+        self.relu = nn.LeakyReLU()
+        self.sigmoid = nn.Sigmoid()
+        self.interpolate = partial(F.interpolate, scale_factor=2, mode='bilinear')
+
+    def forward(self, z, audio):
+        image = self.conv1(z)
+        image = self.relu(image)
+        image = self.interpolate(image)
+        image = self.conv2(image)
+        image = self.sigmoid(image)
+
+        return image
 
 
 class Autoencoder(nn.Module):
