bf7a07b8e17000cda5b1d60566cbd646768e8e55
kureta <skureta@gmail.com>
2019-04-24 23:35:40 +0300

    Some problems wit h DataLoader multiprocess

--- berlin/dataset/audio.py
+++ berlin/dataset/audio.py
@@ -1,6 +1,47 @@
-from berlin.dataset import BaseDataset
+import os
+
+import essentia.standard as es
+import numpy as np
+from numpy.lib.stride_tricks import as_strided
+from torch.utils.data import Dataset
+
+VIDEO_DIR = '/home/kureta/Videos/Rendered/'
+VIDEO_FILE_NAME = '{}.mov'
+AUDIO_FILE_NAME = '{}.wav'
+NUM_VIDEOS = 9
 
 
-class AudioDataset(BaseDataset):
-    def __init__(self, path):
-        super(AudioDataset, self).__init__(path)
+class Audio(Dataset):
+    def __init__(self, video_idx):
+        super(Audio, self).__init__()
+
+        audio_path = os.path.join(VIDEO_DIR, AUDIO_FILE_NAME.format(video_idx + 1))
+
+        self.label = video_idx
+        easy_loader = es.EasyLoader(filename=audio_path,
+                                    replayGain=0,
+                                    sampleRate=48000)
+
+        self.window = es.Windowing(size=2048)
+        self.spectrum = es.Spectrum(size=2048)
+        self.mel = es.MelBands(inputSize=1025, highFrequencyBound=12000, lowFrequencyBound=46.875, numberBands=32,
+                               sampleRate=48000)
+
+        self.audio = easy_loader()
+        self.length = len(self.audio) // (48000 // 25)
+        self.audio = np.pad(self.audio, 64, 'reflect')
+        self.frames = as_strided(self.audio,
+                                 (self.length, 2048),
+                                 ((48000 // 25) * self.audio.strides[0], self.audio.strides[0]),
+                                 writeable=False)
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, idx):
+        value = self.frames[idx]
+        value = self.window(value)
+        value = self.spectrum(value)
+        value = self.mel(value)
+
+        return value, self.label
--- berlin/dataset/video.py
+++ berlin/dataset/video.py
@@ -1,7 +1,6 @@
 import os
 
 import cv2
-import essentia.standard as es
 import torch
 from torch.utils.data import Dataset
 
@@ -12,7 +11,7 @@ NUM_VIDEOS = 9
 
 
 class Video(Dataset):
-    def __init__(self, video_idx):
+    def __init__(self, video_idx, size=(1920, 1080)):
         super(Video, self).__init__()
 
         video_path = os.path.join(VIDEO_DIR, VIDEO_FILE_NAME.format(video_idx + 1))
@@ -20,18 +19,7 @@ class Video(Dataset):
         self.label = video_idx
         self.cap = cv2.VideoCapture(video_path)
         self.length = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        self.__downsample_factor = 1
-
-    @property
-    def downsample_factor(self):
-        return self.__downsample_factor
-
-    @downsample_factor.setter
-    def downsample_factor(self, value):
-        if 120 % value != 0:
-            raise ValueError
-
-        self.__downsample_factor = value
+        self.size = size
 
     def __len__(self):
         return self.length
@@ -39,32 +27,13 @@ class Video(Dataset):
     def __getitem__(self, idx):
         self.cap.set(cv2.CAP_PROP_POS_FRAMES, idx)
         result, frame = self.cap.read()
+
         if not result:
             raise IndexError
 
-        image = cv2.resize(frame, (1024, 1024), interpolation=cv2.INTER_LINEAR)
-        image = torch.from_numpy((image / 255.).astype('float32'))
+        frame = cv2.resize(frame, self.size, interpolation=cv2.INTER_LINEAR)
+        frame = torch.from_numpy((frame / 255 * 2 - 1).astype('float32'))
+        frame = frame.permute((2, 0, 1))
+        # frame = interpolate(frame.unsqueeze(0), self.size, mode='bilinear').squeeze(0)
 
-        return image, self.label
-
-
-class Audio(Dataset):
-    def __init__(self, video_idx):
-        super(Audio, self).__init__()
-
-        audio_path = os.path.join(VIDEO_DIR, AUDIO_FILE_NAME.format(video_idx + 1))
-
-        self.label = video_idx
-        easy_loader = es.EasyLoader(filename=audio_path,
-                                    replayGain=0,
-                                    sampleRate=48000)
-        self.audio = easy_loader()
-        self.length = len(self.audio)
-
-    def __len__(self):
-        return self.length
-
-    def __getitem__(self, idx):
-        value = self.audio[idx]
-
-        return value, self.label
+        return frame, self.label
