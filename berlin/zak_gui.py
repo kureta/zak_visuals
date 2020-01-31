6bb0328470cb5930cabb18e3aca8ee03cb934c8e
kureta <skureta@gmail.com>
2020-02-01 01:54:21 +0300

    Added missing requirement
    Also, cleaned up TODOs.

--- berlin/zak_gui.py
+++ berlin/zak_gui.py
@@ -26,6 +26,11 @@ def resample(x, n, kind='cubic', axis=0):
 # TODO: Rename all the things!
 # TODO: We will need multi-input processor nodes.
 # TODO: Animate latent space exploration in BigGAN
+#       * Create a single latent vector.
+#       * When an animation is triggered, create a second latent vector,
+#       * interpolate between these two vectors, when animation reaches its final frame, stop.
+#       * Put final frame into the first frame.
+#       * Do the same for labels. But instead of random, start and end labels will be selected from TouchOSC.
 # TODO: Create 2 OSCClients
 #       One will relay unhandled messages to Onur
 #       Other will send initialization values to TouchOSC
